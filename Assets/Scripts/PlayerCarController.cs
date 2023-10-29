19de43a18c722259da78ad151f0e8b02150e6926
nickQueens <146036623+nickQueens@users.noreply.github.com>
2023-10-29 19:31:09 +0000

    Basic car control

--- Assets/Scripts/PlayerCarController.cs
+++ Assets/Scripts/PlayerCarController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerCarController : MonoBehaviour
 {
+
+    private float forwardInput;
+    private float horizontalInput;
+
+    public float forwardSpeed = 6;
+    private float turnSpeed = 50;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,11 @@ public class PlayerCarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * forwardSpeed * forwardInput * Time.deltaTime);
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
+
 }
