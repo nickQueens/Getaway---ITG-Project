5357a2a86379bba4fdaea80632fa36caa2453eb6
nickQueens <146036623+nickQueens@users.noreply.github.com>
2023-10-29 20:09:20 +0000

    Implementing player/car control

--- Assets/Scripts/PlayerCarController.cs
+++ Assets/Scripts/PlayerCarController.cs
@@ -8,7 +8,11 @@ public class PlayerCarController : MonoBehaviour
     private float forwardInput;
     private float horizontalInput;
 
-    public float forwardSpeed = 6;
+    private float forwardAcceleration = 1;
+    private float forwardSpeed = 0;
+    private float dragCoefficient = 0.04f;
+    private float minimumSpeed = 0.5f;
+
     private float turnSpeed = 50;
     // Start is called before the first frame update
     void Start()
@@ -20,10 +24,25 @@ public class PlayerCarController : MonoBehaviour
     void Update()
     {
         forwardInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * forwardSpeed * forwardInput * Time.deltaTime);
+        float drag = forwardSpeed * -dragCoefficient;
+        float acceleration = forwardInput * forwardAcceleration;
+        forwardSpeed += acceleration + drag;
+        transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime);
 
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        if (forwardSpeed != 0)
+        {
+            // This feels unnatural when playing
+            // ToDo: Use physics to add sideways force to the front of the car to turn
+            horizontalInput = Input.GetAxis("Horizontal");
+            transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+            if ((Mathf.Abs(forwardSpeed) < minimumSpeed) && (forwardInput == 0))
+            {
+                forwardSpeed = 0;
+            }
+        }
+
+        Debug.Log("Car speed: " + forwardSpeed);
     }
 
 }
