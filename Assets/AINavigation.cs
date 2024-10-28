77be858a3bc552a7b178227f7b33677a9c4c8382
nickQueens <146036623+nickQueens@users.noreply.github.com>
2024-10-28 22:46:50 +0000

    Logic for AI to find player

--- Assets/AINavigation.cs
+++ Assets/AINavigation.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
+using static UnityEditor.PlayerSettings;
 
 public class AINavigation : MonoBehaviour
 {
@@ -9,6 +12,7 @@ public class AINavigation : MonoBehaviour
     private float maxDistanceFromParent = 50;
     [SerializeField] GameObject targetObject;
     [SerializeField] GameObject parentObject;
+    private float distanceToPlayer = float.PositiveInfinity;
     public Vector3 directionToTarget;
     // Start is called before the first frame update
     void Start()
@@ -23,6 +27,19 @@ public class AINavigation : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        var allPlayers = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (var player in allPlayers)
+        {
+            var d = (player.transform.position - transform.position).sqrMagnitude;
+            if (d < distanceToPlayer)
+            {
+                targetObject = player;
+                distanceToPlayer = d;
+            }
+        }
+
+        if (targetObject == null) { return; }
         if (Vector3.Distance(transform.position, parentObject.transform.position) < maxDistanceFromParent)
         {
             agent.SetDestination(targetObject.transform.position);
