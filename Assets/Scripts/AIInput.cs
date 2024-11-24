24b446fbc42c5751ed49ae1e9842aa7686464de5
nickQueens <146036623+nickQueens@users.noreply.github.com>
2024-11-24 20:17:44 +0000

    Adding waypoint logic to AI controller

--- Assets/Scripts/AIInput.cs
+++ Assets/Scripts/AIInput.cs
@@ -1,9 +1,17 @@
 using Unity.Netcode.Components;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AIInput : MonoBehaviour
 {
-    private Transform targetTransform = null;
+    [SerializeField]
+    private bool isInPursuit = false;
+    [SerializeField]
+    private bool isTraffic = true;
+    [SerializeField]
+    private Waypoint currentWaypoint;
+
+    private Transform targetTransform;
     private TireController tireController;
     private new Rigidbody rigidbody;
 
@@ -12,16 +20,26 @@ public class AIInput : MonoBehaviour
     private bool handbrakeOn = false;
 
     private const float pursuitAcceleration = 0.6f;
+    private const float trafficAcceleration = 0.4f;
     private const float reverseAcceleration = -0.4f;
     private const float reverseDistance = 9f;
     private const float turnThreshold = 0.02f;
-    private const float maxSpeed = 25f;
+    private const float pursuitMaxSpeed = 25f;
+    private const float trafficMaxSpeed = 5f;
+
+    private float currentMaxSpeed;
+    private float currentAcceleration;
+
 
     private void Awake()
     {
         tireController = GetComponent<TireController>();
-        targetTransform = transform.parent.Find("NavAgent");
         rigidbody = transform.GetComponent<Rigidbody>();
+
+        targetTransform = isInPursuit ? transform.parent.Find("NavAgent")
+            : isTraffic ? currentWaypoint.transform : null;
+        currentMaxSpeed = isInPursuit ? pursuitMaxSpeed : trafficMaxSpeed;
+        currentAcceleration = isInPursuit ? pursuitAcceleration : trafficAcceleration;
     }
 
     void FixedUpdate()
@@ -46,11 +64,17 @@ public class AIInput : MonoBehaviour
 
         // The above means that we'll have 3 different movement scenarios, chasing, avoiding obstacles & reversing when stuck
 
-        followTarget(targetTransform.position);
+        UpdateWaypoints(currentWaypoint);
+        FollowTarget(targetTransform);
     }
 
-    private void followTarget (Vector3 targetPosition)
+    private void FollowTarget (Transform targetTransform)
     {
+        if (targetTransform == null) {
+            tireController.SetInputs(0, 0, false);
+            return;
+        }
+        Vector3 targetPosition = targetTransform.position;
         float distanceToTarget = Vector3.Distance(transform.position, targetPosition);
         Vector3 directionToTarget = (targetPosition - transform.position).normalized;
         float dot = Vector3.Dot(transform.forward, directionToTarget);
@@ -58,26 +82,26 @@ public class AIInput : MonoBehaviour
         if (dot > 0)
         {
             // Target in front
-            accelerationInput = pursuitAcceleration;
+            accelerationInput = currentAcceleration;
         }
         else
         {
             // Target behind
             if (distanceToTarget > reverseDistance)
             {
-                accelerationInput = pursuitAcceleration;
+                accelerationInput = currentAcceleration;
             }
             else
             {
                 accelerationInput = reverseAcceleration;
             }
         }
-        accelerationInput = rigidbody.velocity.magnitude >= maxSpeed ? 0 : accelerationInput;
+        accelerationInput = rigidbody.velocity.magnitude >= currentMaxSpeed ? 0 : accelerationInput;
 
         if (Mathf.Abs(dot) < (1 - turnThreshold))
         {
             float angleToDirection = Vector3.SignedAngle(transform.forward, directionToTarget, Vector3.up);
-            float speedFactor = Mathf.Clamp01(rigidbody.velocity.magnitude / maxSpeed);
+            float speedFactor = Mathf.Clamp01(rigidbody.velocity.magnitude / pursuitMaxSpeed);
             horizontalInput = angleToDirection > 0 ? 1 - (dot * speedFactor) : -(1 - (dot * speedFactor));
 
             horizontalInput = Mathf.Clamp(horizontalInput, -0.8f, 0.8f);
@@ -88,5 +112,16 @@ public class AIInput : MonoBehaviour
         }
 
         tireController.SetInputs(accelerationInput, horizontalInput, handbrakeOn);
+    }
+
+    private void UpdateWaypoints(Waypoint currentWaypoint)
+    {
+        if (currentWaypoint == null) {  return; }
+        float distance = Vector3.Distance(transform.position, currentWaypoint.transform.position);
+        if (distance < currentWaypoint.width)
+        {
+            this.currentWaypoint = currentWaypoint.nextWaypoint;
+            targetTransform = this.currentWaypoint.transform;
+        }
     }
 }
